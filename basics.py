9d69f14ff9d39a2375e30fc4d5146040f8de8ec3
LinguList <mattis.list@posteo.de>
2014-09-16 09:01:07 +0200

    updated burmish data

--- basics.py
+++ basics.py
@@ -1,7 +1,7 @@
 # author   : Johann-Mattis List
 # email    : mattis.list@uni-marburg.de
 # created  : 2014-09-10 19:49
-# modified : 2014-09-10 19:49
+# modified : 2014-09-10 21:17
 """
 Wordlist plugin for burmish data.
 """
@@ -9,7 +9,66 @@ Wordlist plugin for burmish data.
 __author__="Johann-Mattis List"
 __date__="2014-09-10"
 
+import unicodedata as ucd
+import re
+import sqlite3
 import lingpyd
+
+from .unicode import *
+
+
+def clean_entry(entry, **keywords):
+    """
+    Normalize (NFC) entry and remove bad chars.
+    """
+
+    kw = dict(
+            brackets = rcParams['brackets'],
+            exact_bracket_matching = True,
+            )
+    kw.update(keywords)
+
+    # normalize first
+    new_entry = ucd.normalize("NFD", entry)
+
+    # normalize linguistically
+    entries = list(new_entry)
+    for i,char in enumerate(entries):
+        try:
+            entries[i] = rcParams['normalizations'][char]
+        except KeyError:
+            pass
+    new_entry = ''.join(entries)
+    
+    if kw['exact_bracket_matching']:
+        # delete stuff in brackets
+        for b1 in kw['brackets']:
+            b2 = get_pendant(b1)
+            
+            # get possible index
+            idxA = new_entry.find(b1)
+            idxB = new_entry.find(b2)
+
+            # check for existing indices
+            if idxA != -1 and idxA < idxB:
+                new_entry = new_entry[:idxA]+new_entry[idxB+1:]
+    else:
+        b1s = []
+        b2s = []
+        for b1 in kw['brackets']:
+            idxA = new_entry.find(b1)
+            if idxA != -1:
+                b1s.append(idxA)
+            idxB = new_entry.find(get_pendant(b1))
+            if idxB != -1:
+                b2s.append(idxB)
+
+        new_entry = new_entry[:min(b1s)]+new_entry[max(b2s)+1:]
+
+    # go for spaces and replace by '_'
+    new_entry = new_entry.replace(' ','_')
+    
+    return new_entry
 
 def ipa2tokens(
         istring,
@@ -85,6 +144,9 @@ def ipa2tokens(
             )
     kw.update(keywords)
 
+    # clean the entry first
+    istring = clean_entry(istring)
+
     # check for pre-tokenized strings
     if ' ' in istring:
         out = istring.split(' ')
@@ -97,16 +159,25 @@ def ipa2tokens(
     out = []
 
     nasals = "ãũẽĩõ"
-    nasal = "\u0303"
-    semi_diacritics = "ʃhsʑɕʂʐ"
+    nasal_char = "\u0303"
+    semi_diacritics = "ʃhsʑɕʂʐñ"
+    nogos = '_'
     
     # set basic characteristics
     vowel = False # no vowel
     tone = False # no tone
     merge = False # no merge command
     start = True # start of unit
+    nasal = False # start of nasal vowel environment
 
     for char in istring:
+
+        # check for nasal stack and vowel environment
+        if nasal:
+            if char not in kw['vowels'] and char not in kw['diacritics'] :
+                out += [rcParams['nasal_placeholder']]
+                nasal = False
+                
         # check for breaks first, since they force us to start anew
         if char in kw['breaks']:
             start = True
@@ -137,21 +208,13 @@ def ipa2tokens(
             merge = False
 
         # check for nasals 
-        elif kw['expand_nasals'] and char == nasal:
+        elif kw['expand_nasals'] and char == nasal_char and vowel:
             out[-1] += char
-            out += ["∼"]
             start = False
-            merge = False
-
-        # check for nasal vowels
-        elif char in nasals:
-            out += [char]
-            out += ["∼"]
-            start = False
-            merge = False
+            nasal = True
 
         # check for weak diacritics
-        elif char in semi_diacritics and not start and not vowel:
+        elif char in semi_diacritics and not start and not vowel and not tone and out[-1] not in nogos:
             out[-1] += char
         
         # check for diacritics
@@ -190,8 +253,60 @@ def ipa2tokens(
             out += [char]
             start = False
             tone = False
-
+    
+    if nasal:
+        out += [rcParams['nasal_placeholder']]
+        
     return out
+
+def secondary_structures(tokens):
+    """
+    Function handles the tokenization of strings into secondary structures.
+    """
+    segment = rcParams['morpheme_separator']
+
+    pstring = lingpyd.prosodic_string(tokens)
+    
+    # check for more than one vowel in the set
+    vlen = pstring.count('X')+pstring.count('Z')+pstring.count('Y')
+    if vlen == 1: return tokens
+    elif vlen == 2 and rcParams['nasal_placeholder'] in tokens:
+        return tokens
+
+    out = []
+    tmp_tokens = list(zip(tokens, 
+        lingpyd.prosodic_string(tokens)
+        ))
+    
+    new_syllable = True
+    while tmp_tokens:
+
+        token,prochar = tmp_tokens.pop(0)
+        
+        # check for tonal pro-chars
+        if prochar == 'T'  and len(tmp_tokens) != 0:
+            out += [token, segment]
+            new_syllable = True
+
+        elif prochar == '_' and tmp_tokens and not new_syllable:
+            out += [segment]
+            new_syllable = True
+
+        # check for markers if no tone is given
+        elif prochar == 'B' and not new_syllable and tmp_tokens:
+            if out[-1] == 'A':
+                out += [token]
+            else:
+                out += [segment,token]
+
+        # if nothing else is given, just append the string
+        else:
+            out += [token]
+            new_syllable = False
+    
+    return out
+
+
 
 class Wordlist(lingpyd.basic.wordlist.Wordlist):
 
@@ -207,5 +322,35 @@ class Wordlist(lingpyd.basic.wordlist.Wordlist):
     def tokenize(self, override=True):
 
         self.add_entries('tokens', 'ipa', lambda x:
-                ipa2tokens(x.replace(' ','_'),override=override))
+                ipa2tokens(x.replace(' ','_')),override=override)
+
+        self.add_entries('prostring','tokens', lambda x: lingpyd.prosodic_string(x,
+            _output='CcV'), override)
+
+
+        self.add_entries('tokens', 'tokens', lambda x: secondary_structures(x),
+                override = override)
+    
+    def update(self, dbase, table, ignore=False):
+        """
+        Upload triple-data to sqlite3-db.
+        """
+        if not ignore: ignore=[]
+        # get the triples
+        triples = lingpyd.basic.ops.tsv2triple(self,False)
+        
+        # connect to tatabase
+        db = sqlite3.connect(dbase)
+        cursor = db.cursor();
+        cursor.execute('drop table '+table+';')
+        cursor.execute('create table '+table+' (ID int, COL text, VAL text);')
+        cursor.execute('vacuum')
+        for a,b,c in triples:
+            if b.lower() not in ignore:
+                if type(c) == list:
+                    c = ' '.join([str(x) for x in c])
+                else:
+                    c = str(c)
+                cursor.execute('insert into '+table+' values (?, ?, ?);', (a, b, c))
+        db.commit()
 
